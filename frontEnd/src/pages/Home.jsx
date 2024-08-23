d0338f09ec43c8790af1aa39d242fc52d0c848fc
bipulsingh126 <bipulpratqpsingh126@gmail.com>
2024-08-23 16:37:34 +0530

    frontend commit

---
+++ frontEnd/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout.jsx';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Home page</h1>
+    </Layout>
+  )
+}
+
+export default Home
