2e9586d3425d5f83ce80255fe065dea376ff69be
bipulsingh126 <bipulpratqpsingh126@gmail.com>
2024-08-25 14:14:14 +0530

    third

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,26 +1,22 @@
-
-import {Route, Routes} from "react-router-dom"
-import Home from "./pages/Home.jsx"
-import About from "./pages/About.jsx"
-import Contact from "./pages/Contact.jsx"
-import Policy from "./pages/Policy.jsx"
-import PageNotFound from "./pages/PageNotFound.jsx"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Policy from "./pages/Policy.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
- 
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/policy" element={<Policy/>} />
-      <Route path="*" element={<PageNotFound/>} />
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
